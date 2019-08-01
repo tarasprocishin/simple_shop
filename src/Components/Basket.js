f33cbd8f90ff644f00305f16e2cb42e51bbf8260
tarasprocishin <taras1procishin@gmail.com>
2019-08-01 22:33:26 +0300

    Start implementing main basket

--- src/Components/Basket.js
+++ src/Components/Basket.js
@@ -6,19 +6,30 @@ class Basket extends React.Component{
         this.state = {
             toggle: false
         }
-
     }
+    goToBasket = (el) => {
+        this.setState(prevState => (
+           {toggle: !prevState.toggle} 
+        ))
+    }
+
+  
     render() {
         let {data, basket} = this.props;
     
         // console.timeLog(basket)
          return(
              <div className ="basket">
+                 <div className="basket__icon">
+                 <button onClick={this.goToBasket}> go to basket</button>
                  <h1>
                  {basket.reduce((sum, el) => {
                      return sum + el.number;
                  }, 0 )}
-                 </h1>  
+                 </h1> 
+                 </div>
+                 {this.state.toggle ? <p>Hello world!</p>: null}
+ 
              </div>
         )}
    
