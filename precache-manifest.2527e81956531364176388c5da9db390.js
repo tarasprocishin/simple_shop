1d5eb68cecd268b6920e5aaf95d1b11616363add
tarasprocishin <taras1procishin@gmail.com>
2019-08-05 18:24:47 +0300

    Updates

---
+++ precache-manifest.2527e81956531364176388c5da9db390.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70eebc0efeef52f0bf853af6fe8d2d32",
+    "url": "/simple_shop/index.html"
+  },
+  {
+    "revision": "bf6e29434106e0a4257b",
+    "url": "/simple_shop/static/css/main.31df4810.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/simple_shop/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bf6e29434106e0a4257b",
+    "url": "/simple_shop/static/js/main.eb62e891.chunk.js"
+  },
+  {
+    "revision": "a4ac16d3edbc8884a081",
+    "url": "/simple_shop/static/js/runtime~main.b653f5d1.js"
+  },
+  {
+    "revision": "2bbf7a1cacf4491623c1a95adbd4b9af",
+    "url": "/simple_shop/static/media/fontello.2bbf7a1c.woff2"
+  },
+  {
+    "revision": "3fe0943c722a63c2b7674689e0f83ef1",
+    "url": "/simple_shop/static/media/fontello.3fe0943c.eot"
+  },
+  {
+    "revision": "516988b30a57fe491f70db38af0dd021",
+    "url": "/simple_shop/static/media/fontello.516988b3.svg"
+  },
+  {
+    "revision": "f849628e48ffd28077dfc552e56dbc8f",
+    "url": "/simple_shop/static/media/fontello.f849628e.woff"
+  },
+  {
+    "revision": "f8b504e6ab8cf13dea768c5cbf964f3f",
+    "url": "/simple_shop/static/media/fontello.f8b504e6.ttf"
+  }
+]);
